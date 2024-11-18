18d2aa7189dc92182655e7dca5624c68e641ca8f
Akash Ghosh <akashghoshofficial700@gmail.com>
2024-11-18 18:17:57 +0530

    delete dist folder

--- dist/services/media.js
+++
@@ -1,6 +0,0 @@
-import { Hono } from "hono";
-import { allMedia, uploadMedia } from "../controllers/media_controller";
-const app = new Hono();
-app.get("/all", allMedia);
-app.post("/upload", uploadMedia);
-export default app;
